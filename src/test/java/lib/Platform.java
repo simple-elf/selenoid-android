a8f7ab47735e0462fbd838aad5fe731496d6a11d
Aaftakhov <simple1elf@gmail.com>
2018-07-01 22:52:19 +0500

    Url and capabilities

--- src/test/java/lib/Platform.java
+++ src/test/java/lib/Platform.java
@@ -3,7 +3,9 @@ package lib;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
 
@@ -11,7 +13,7 @@ public class Platform {
 
     private static final String PLATFORM_IOS = "ios";
     private static final String PLATFORM_ANDROID = "android";
-    private static final String APPIUM_URL = "http://192.168.1.204:4723/wd/hub"; // "http://127.0.0.1:4723/wd/hub"
+    private static final String APPIUM_URL = "http://46.101.105.58:4444/wd/hub"; // "http://127.0.0.1:4723/wd/hub"
 
     private static Platform instance;
     private Platform() {}
@@ -23,11 +25,12 @@ public class Platform {
         return instance;
     }
 
-    public AppiumDriver getDriver() throws Exception {
+    public WebDriver getDriver() throws Exception {
         URL url = new URL(APPIUM_URL);
 
         if (this.isAndroid()) {
-            return new AndroidDriver(url, this.getAndroidDesiredCapabilities());
+            return new RemoteWebDriver(url, this.getAndroidDesiredCapabilities());
+            //return new AndroidDriver(url, this.getAndroidDesiredCapabilities());
         } else if (this.isIOS()) {
             return new IOSDriver(url, this.getIOSDesiredCapabilities());
         } else {
@@ -46,14 +49,19 @@ public class Platform {
     private DesiredCapabilities getAndroidDesiredCapabilities() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","6.0");
-        capabilities.setCapability("automationName","Appium");
+        //capabilities.setCapability("platformName", "Android");
+        //capabilities.setCapability("deviceName","AndroidTestDevice");
+        //capabilities.setCapability("platformVersion","6.0");
+        //capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
-        //capabilities.setCapability("app","C:\\Users\\simpl\\IdeaProjects\\JavaAppiumAutomation\\apks\\org.wikipedia.apk"); // for Windows
-        capabilities.setCapability("app","/Users/simple1elf/JavaAppium/apks/org.wikipedia.apk"); //for MacOs
+        capabilities.setCapability("app","D:\\WORK\\IdeaProjects\\selenoid-android\\apks\\org.wikipedia.apk"); // for Windows
+        //capabilities.setCapability("app","/Users/simple1elf/JavaAppium/apks/org.wikipedia.apk"); //for MacOs
+
+        capabilities.setCapability("browserName", "android");
+        capabilities.setCapability("version","6.0");
+        //capabilities.setCapability("enableVNC",true);
+        //capabilities.setCapability("enableVideo",true);
 
         return capabilities;
     }
