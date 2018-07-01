895183d0b992eb5e975697fb08c14c674fc67553
Aaftakhov <simple1elf@gmail.com>
2018-07-01 22:51:42 +0500

    Android Demo tests from https://github.com/vania-pooh/demo-test

---
+++ src/test/java/tests/DemoTest.java
@@ -0,0 +1,53 @@
+package tests;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.net.URL;
+
+public class DemoTest {
+
+    private RemoteWebDriver driver;
+
+    @Before
+    public void openDriver() throws Exception {
+        final DesiredCapabilities browser = DesiredCapabilities.chrome();
+        browser.setCapability("enableVNC", true);
+        browser.setCapability("enableVideo", true);
+        driver = new RemoteWebDriver(new URL(
+                "http://46.101.105.58:4444/wd/hub" //Replace with correct host and port
+        ), browser);
+    }
+
+    @Test
+    public void browserTest() throws Exception {
+        try {
+            driver.get("http://duckduckgo.com/");
+            WebElement input = driver.findElement(By.cssSelector("input#search_form_input_homepage"));
+            input.sendKeys(Keys.chord("selenium", Keys.ENTER));
+        } finally {
+            takeScreenshot(driver);
+        }
+
+    }
+
+    static void takeScreenshot(RemoteWebDriver driver) throws Exception {
+        byte[] screen = ((TakesScreenshot) new Augmenter().augment(driver)).getScreenshotAs(OutputType.BYTES);
+        FileUtils.writeByteArrayToFile(new File(driver.getSessionId() + ".png"), screen);
+    }
+
+    @After
+    public void closeDriver(){
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
